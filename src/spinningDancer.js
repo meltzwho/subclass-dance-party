82082ff9a0291720499e2f067d6000d05056e8b4
meltzwho <etmeltzer@live.com>
2018-06-18 19:41:00 -0400

    Added spinningDancer class

---
+++ src/spinningDancer.js
@@ -0,0 +1,11 @@
+var makeSpinningDancer = function(top, left, timeBetweenSteps) {
+    makeDancer.call(this, top, left, timeBetweenSteps, $('<img class="kitten" src="https://placekitten.com/50/50">'));
+  
+  };
+  makeSpinningDancer.prototype = Object.create(makeDancer.prototype);
+  makeSpinningDancer.prototype.constructor = makeSpinningDancer;
+  
+  makeSpinningDancer.prototype.step = function() {
+    //makeDancer.prototype.step.bind(this)();
+    //this.$node.toggle();
+  };
